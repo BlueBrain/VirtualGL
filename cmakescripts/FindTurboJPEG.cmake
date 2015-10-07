130c38e59eefdab58c15616781975389ece473e1
Juan Hernando <jhernando@fi.upm.es>
2015-10-07 12:53:06 +0200

    Improved finding of libturbojpeg

--- cmakescripts/FindTurboJPEG.cmake
+++ cmakescripts/FindTurboJPEG.cmake
@@ -8,29 +8,18 @@ else()
 	unset(TJPEG_INCLUDE_DIR CACHE)
 endif()
 
-find_path(TJPEG_INCLUDE_DIR turbojpeg.h
-	DOC "TurboJPEG include directory (default: ${DEFAULT_TJPEG_INCLUDE_DIR})"
-	HINTS ${DEFAULT_TJPEG_INCLUDE_DIR})
-if(TJPEG_INCLUDE_DIR STREQUAL "TJPEG_INCLUDE_DIR-NOTFOUND")
-	message(FATAL_ERROR "Could not find turbojpeg.h in ${DEFAULT_TJPEG_INCLUDE_DIR}.  If it is installed in a different place, then set TJPEG_INCLUDE_DIR accordingly.")
-else()
-	message(STATUS "TJPEG_INCLUDE_DIR = ${TJPEG_INCLUDE_DIR}")
-endif()
+# Include dir
+find_path(TJPEG_INCLUDE_DIR
+  NAMES turbojpeg.h
+  PATHS /opt/libjpeg-turbo/include
+)
+# Finally the library itself
+find_library(TJPEG_LIBRARY
+  NAMES libturbojpeg.so libturbojpeg.so.0 turbojpeg.dll libturbojpeg.dylib
+  PATHS ${LibJpegTurbo_PKGCONF_LIBRARY_DIRS} /opt/libjpeg-turbo/lib
+)
 include_directories(${TJPEG_INCLUDE_DIR})
 
-set(DEFAULT_TJPEG_LIBRARY /opt/libjpeg-turbo/lib${BITS}/libturbojpeg.a)
-
-set(TJPEG_LIBRARY ${DEFAULT_TJPEG_LIBRARY} CACHE STRING
-	"Path to TurboJPEG library or flags necessary to link with it (default: ${DEFAULT_TJPEG_LIBRARY})")
-
-set(CMAKE_REQUIRED_INCLUDES ${TJPEG_INCLUDE_DIR})
-set(CMAKE_REQUIRED_LIBRARIES ${TJPEG_LIBRARY})
-check_c_source_compiles("#include <turbojpeg.h>\nint main(void) { tjhandle h=tjInitCompress(); return 0; }" TURBOJPEG_WORKS)
-set(CMAKE_REQUIRED_DEFINITIONS)
-set(CMAKE_REQUIRED_INCLUDES)
-set(CMAKE_REQUIRED_LIBRARIES)
-if(NOT TURBOJPEG_WORKS)
-	message(FATAL_ERROR "Could not link with TurboJPEG library ${TJPEG_LIBRARY}.  If it is installed in a different place, then set TJPEG_LIBRARY accordingly.")
-endif()
-
-message(STATUS "TJPEG_LIBRARY = ${TJPEG_LIBRARY}")
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(TURBOJPEG DEFAULT_MSG
+       TJPEG_LIBRARY TJPEG_INCLUDE_DIR)
