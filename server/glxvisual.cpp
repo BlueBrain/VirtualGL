BlueBrain/VirtualGL

commit 9fe3458951a076b3b269e88ad5ca67874bb10b75

Ensure that the FB config chosen in glXChooseVisual() and glXChooseFBConfig() is suitable for both Pixmap and Pbuffer rendering, because regardless of the value of VGL_DRAWABLE, VirtualGL will always use Pixmaps on the 3D X server to represent Pixmaps on the 2D X server (necessary to make GLX_EXT_texture_from_pixmap work.)  nVidia drivers specifically export different Pbuffer-enabled and Pbuffer/Pixmap-enabled FB configs, so if we don't do this, then the "real" version of glXMakeCurrentContext() may fail when VGL attempts to make a Pixmap-backed virtual drawable current (because the context was created with an FB config that is not Pixmap-compatible.)
git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@3647 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

server/glxvisual.cpp
@@ -1,6 +1,6 @@
 /* Copyright (C)2004 Landmark Graphics Corporation
  * Copyright (C)2005 Sun Microsystems, Inc.
- * Copyright (C)2009-2014 D. R. Commander
+ * Copyright (C)2009-2015 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -274,7 +274,7 @@ GLXFBConfig *configsFromVisAttribs(const int attribs[],
 	if(fconfig.drawable==RRDRAWABLE_PIXMAP)
 		glxattribs[j++]=GLX_PIXMAP_BIT|GLX_WINDOW_BIT;
 	else
-		glxattribs[j++]=GLX_PBUFFER_BIT;
+		glxattribs[j++]=GLX_PIXMAP_BIT|GLX_PBUFFER_BIT;
 	glxattribs[j++]=GLX_X_VISUAL_TYPE;  glxattribs[j++]=GLX_TRUE_COLOR;
 	glxattribs[j]=None;
 

commit 5759895a164616eedc685c23da8e00c4a90eb29f

Hotfix for a regression in VGL_SAMPLES introduced in 9fe3458.

server/glxvisual.cpp
@@ -263,7 +263,12 @@ GLXFBConfig *configsFromVisAttribs(const int attribs[],
 	if(fconfig.drawable==RRDRAWABLE_PIXMAP)
 		glxattribs[j++]=GLX_PIXMAP_BIT|GLX_WINDOW_BIT;
 	else
-		glxattribs[j++]=GLX_PIXMAP_BIT|GLX_PBUFFER_BIT;
+	{
+        if (samples==-1)
+            glxattribs[j++]=GLX_PIXMAP_BIT|GLX_PBUFFER_BIT;
+        else
+            glxattribs[j++]=GLX_PBUFFER_BIT;
+	}
 	glxattribs[j++]=GLX_X_VISUAL_TYPE;  glxattribs[j++]=visualType;
 	glxattribs[j]=None;
 